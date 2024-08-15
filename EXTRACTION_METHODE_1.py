feac581c115f5ef5118e6c9b9423f475c07dfc44
Maël <116744313+Mael45000@users.noreply.github.com>
2024-08-15 12:03:40 +0200

    Update EXTRACTION_METHODE_1.py

--- EXTRACTION_METHODE_1.py
+++ EXTRACTION_METHODE_1.py
@@ -5,6 +5,7 @@ import re
 lexique_csv = pd.read_csv("C:/Users/Utilisateur/Downloads/Lexique-query-2024-07-18 11-58-45.csv", sep=";")
 corpus_csv = pd.read_csv("C:/Users/Utilisateur/Documents/EXTRAIT_CORPUS_TREMOLO.csv", sep=";", encoding="UTF-8", engine='python')
 
+# Crée une variable avec une entrée contenant une châine de caractère, qui retourne une chaîne de caractères normalisée
 def normalisation(y):
     z=emoji.demojize(y) 
     x=re.sub("#\w+ ?|@XXX:?|RT |:[\w-]+:|url_path|\w+[’']|[^A-Za-zïêëâüçûôéèñàãöùî ]+|\d+\S*","",z)
@@ -14,18 +15,21 @@ def normalisation(y):
  
 lexique = lexique_csv["Word"]
 corpus_csv["Texte_norm"] = corpus_csv["Texte"].apply(normalisation)
+#Crée une colonne qui affiche les tweets normalisés grâce à la variable normalisation crée ultérieurement
 corpus_csv["mots"] = corpus_csv["Texte_norm"].str.split()
 
-# Create a DataFrame with all words, their tweet_id, and tweet text
-all_words_df = corpus_csv.explode("mots")[["Text_index", "Texte", "mots"]]
-all_words_df.columns = ["tweet_id", "tweet", "mot"]
-#total_word_count = all_words.shape[0] permet de compter le nombre total de mots
 
-# Filter words not in the lexicon
+all_words = corpus_csv.explode("mots")[["Text_index", "Texte", "mots"]]
+all_words.columns = ["tweet_id", "tweet", "mot"]
+# total_word_count = all_words.shape[0] ----> permet de compter le nombre total de mots dans le corpus
+
+# Filtre les mots qui ne sont pas dans le lexique
 df_results = all_words_df[~all_words_df["mot"].isin(lexique)]
 
+# Crée un fichier csv qui stocke les mots pas présents dans le corpus
 df_results.to_csv("methode_1_sortie.csv", index=False)
 
 print("Mots absents du lexique :")
 print(df_results)
+# Affiche les mots asbsents du lexique 
 
