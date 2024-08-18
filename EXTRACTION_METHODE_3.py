3558410c49580611a114d72fc92c51f92dce4f97
Maël <116744313+Mael45000@users.noreply.github.com>
2024-08-18 17:48:40 +0200

    Update EXTRACTION_METHODE_3.py

--- EXTRACTION_METHODE_3.py
+++ EXTRACTION_METHODE_3.py
@@ -6,16 +6,16 @@ corpus_csv= pd.read_csv("corpus_tremolo_full.txt", sep='\t', encoding="Windows-1
 texte=corpus_csv['Texte']
 ID=corpus_csv['Text_index']
 f_sortie=open("methode_3_sortie.csv","w",encoding="UTF-8")
-# Crée un fichier qui stockera les phénomènes
+# Crée un fichier qui stocke les phénomènes.
 f_sortie.write("id"+"\t"+"expression"+"\t"+"tweet"+"\n")
 c=-1
 for line in texte:
     c=c+1
     expr_reg=re.search(r"orthographe|\bon dit(?!\squ)|le (terme|mot)|langage",str(line),re.IGNORECASE)
-   # Recherche l'expression régulière dans le tweet
+   # Recherche l'expression régulière dans le tweet. La méthode IGNORECASE permet d'ignorer les cases vides.
     if expr_reg :
         f_sortie.write(ID[c]+"\t"+expr_reg.group()+"\t"+line+"\n")
-        # Si l'expression régulière est présente dans le tweet, on stocke le tweet 
+        # Si l'expression régulière est présente dans le tweet, alors on stocke le tweet. 
         print(expr_reg.group(),line)
          
 f_sortie.close()
