58a84e4cc84d5de03e6828a6a830048ee74f6e98
Maël <116744313+Mael45000@users.noreply.github.com>
2024-08-18 17:40:29 +0200

    Update EXTRACTION_METHODE_1.py

--- EXTRACTION_METHODE_1.py
+++ EXTRACTION_METHODE_1.py
@@ -5,30 +5,34 @@ import re
 lexique_csv = pd.read_csv("Lexique.csv", sep=";")
 corpus_csv = pd.read_csv("EXTRAIT_CORPUS_TREMOLO.csv", sep=";", encoding="UTF-8", engine='python')
 
-# Crée une variable avec une entrée contenant une châine de caractère, qui retourne une chaîne de caractères normalisée
 def normalisation(y):
     z=emoji.demojize(y) 
     x=re.sub("#\w+ ?|@XXX:?|RT |:[\w-]+:|url_path|\w+[’']|[^A-Za-zïêëâüçûôéèñàãöùî ]+|\d+\S*","",z)
     w=x.lstrip()
     v=w.lower()
     return v
+    # Crée une variable avec une entrée contenant une chaîne de caractère et retourne une chaîne de caractères normalisée.
  
 lexique = lexique_csv["Word"]
 corpus_csv["Texte_norm"] = corpus_csv["Texte"].apply(normalisation)
-#Crée une colonne qui affiche les tweets normalisés grâce à la variable normalisation crée ultérieurement
-corpus_csv["mots"] = corpus_csv["Texte_norm"].str.split()
+# Crée une colonne qui affiche les tweets normalisés grâce à la variable normalisation crée ultérieurement.
 
+corpus_csv["mots"] = corpus_csv["Texte_norm"].str.split()
+# Crée une colonne de mots en séparant les phrases dans la colonne des tweets
 
 all_words = corpus_csv.explode("mots")[["Text_index", "Texte", "mots"]]
+# Sépare les listes de mots crée avec la méthode .split afin d'avoir un mot par ligne dans la colonne all_words.
+
 all_words.columns = ["tweet_id", "tweet", "mot"]
-# total_word_count = all_words.shape[0] ----> permet de compter le nombre total de mots dans le corpus
+# Donne un nom aux colonnes
+# total_word_count = all_words.shape[0] ----> permet de compter le nombre total de mots dans le corpus.
 
-# Filtre les mots qui ne sont pas dans le lexique
 resultats = all_words[~all_words["mot"].isin(lexique)]
+# Filtre les mots qui ne sont pas dans le lexique.
 
-# Crée un fichier csv qui stocke les mots pas présents dans le corpus
 resultats.to_csv("methode_TEST_sortie.csv", index=False)
+# Crée un fichier .csv qui stocke les colonnes dans le corpus.
 
 print("Mots absents du lexique :")
 print(resultats)
-# Affiche les mots asbsents du lexique 
+# Affiche les mots asbsents du lexique.
